74b5e9f055eb3ae7ac1fcb739f3f28cd3b61eff3
Sergio Sanchez <surgeserg@hotmail.com>
2019-01-13 21:16:17 -0600

    Initial

---
+++ public/front-end.js
@@ -0,0 +1,9 @@
+const navigation = document.getElementById('side-nav')
+
+const openNav = () => {
+	navigation.style.width = '150px';
+};
+
+const closeNav = () => {
+	navigation.style.width = '0';
+}
